8833acea033847f783f4b3cde33ada2cfe9b1112
Dawit <dawitmisgna4@gmail.com>
2025-07-18 18:04:41 +0300

    main page controller

--- lib/controller/main_page_controller.dart
+++ lib/controller/main_page_controller.dart
@@ -1,11 +1,21 @@
 import 'package:flutter_movie_app/model/main_page_data.dart';
+import 'package:flutter_movie_app/model/movie_model.dart';
 import 'package:flutter_movie_app/services/movie_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 
 class MainPageController extends StateNotifier<MainPageData> {
   MainPageController([MainPageData? state])
-    : super(state ?? MainPageData.initial()) {}
+    : super(state ?? MainPageData.initial()) {
+    getMovies();
+  }
 
+  final MovieService _movieService = GetIt.instance.get<MovieService>();
 
+  Future<void> getMovies() async {
+    try {
+      List<MovieModel> movies = [];
+      movies = await _movieService.getPopularMovies(state.page);
+    } catch (e) {}
+  }
 }
--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_movie_app/model/movie_model.dart';
 import 'package:flutter_movie_app/services/http_service.dart';
 import 'package:get_it/get_it.dart';
 
@@ -5,8 +7,21 @@ class MovieService {
   final GetIt getIt = GetIt.instance;
 
   late HTTPService _http;
-  
+
   MovieService() {
     _http = getIt.get<HTTPService>();
+  }
+
+  Future<List<MovieModel>> getPopularMovies(int? page) async {
+    Response response = await _http.get('/movies/popular', {'page': page});
+
+    if (response.statusCode == 200) {
+      List<MovieModel> movies = ( response.data['results'] as List)
+          .map((movie) => MovieModel.fromJson(movie))
+          .toList();
+      return movies;
+    } else {
+      throw Exception('Failed to load popular movies');
+    }
   }
 }
